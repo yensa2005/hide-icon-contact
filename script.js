d1c98735b6acd8b3e85e7d0fd90fb9b68ba11dbd
Yen Sa <ttysa98tcv@gmail.com>
2021-08-19 11:10:50 +0700

    new

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
-function myFunction() {
-    var x = document.getElementById("myDIV");
-    if (x.style.display === "block") {
-        x.style.display = "none";
+// function myFunction() {
+//     var x = document.getElementById("myDIV");
+//     if (x.style.display === "block") {
+//         x.style.display = "none";
+//     } else {
+//         x.style.display = "block";
+//     }
+// }
+
+// $(document).ready(function() {
+//     $(".chat-btn").click(function() {
+//         $("#myDIV").hide("slow");
+//     });
+
+//     $(".chat-btn").click(function() {
+//         $("#myDIV").show("slow");
+//     });
+// });
+
+$(".chat-btn").click(function() {
+    var x = $("#myDIV");
+    if (x.hasClass('hide')) {
+        x.removeClass('hide').hide("slow");
     } else {
-        x.style.display = "block";
+        x.addClass('hide').show("slow");
     }
-}
+});
